44b15a08359da125d254b96d49e1f0a919d73d45
vjlux <gruber.lukas@gmail.com>
2015-05-03 22:46:45 -0700

    Add boost nuget package

--- cpp/SeePlusPlus/SeePlusPlus/src/Main.cpp
+++ cpp/SeePlusPlus/SeePlusPlus/src/Main.cpp
@@ -1,2 +1,13 @@
-// Lukas Gruber
+// SeePlusPlus
+// Lukas Gruber (gruber.lukas@gmail.com)
 
+#include <iostream> 
+
+int main(int argc, char *argv[])
+{
+  std::cout << "Welcome to SeePlusPlus." << std::endl;
+
+
+
+  std::cout << "Bye bye." << std::endl;
+}
